aa8229c87aa332e04ce21065bfde6cf68e45e4ed
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2024-03-05 13:17:22 -0500

    Adding and updating API ends

--- app.js
+++ app.js
@@ -30,6 +30,39 @@ app.get('/', (req, res) => {
     });
 });
 
+app.post('/add', (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone_number = req.body.phone_number;
+
+    const query = `INSERT INTO contact_info (name, email, phone_number) VALUES ($1, $2, $3)`;
+
+    db.query(query, [name, email, phone_number], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Contact added");
+        }
+    });
+});
+
+app.post('/edit' , (req, res) => {
+    
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone_number = req.body.phone_number;
+
+    const query = `UPDATE contact_info SET email = $1, phone_number = $2 WHERE name = $3`;
+
+    db.query(query, [email, phone_number, name], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Contact updated");
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     db.connect();
